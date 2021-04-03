64af51af2ce481128495d6fef15b00b58bed57e9
Monico Moreno <monico_moreno@collaborare.net>
2021-04-03 17:00:01 -0500

    Added functionality to do minimal, api consumption; add .gitignore

--- starter/routes/index.js
+++ starter/routes/index.js
@@ -1,9 +1,22 @@
 let express = require('express');
 let router = express.Router();
+let nodeFetch = require('node-fetch');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let users = nodeFetch('http://localhost:1337/users')
+    .then((data) => {
+      if (data.status !== 200){
+        return null;
+      }
+      return data.json();
+    })
+    .then((data) => {
+      res.render('index', {
+        title: 'Front End Component of strapi-starter-to-customize',
+        users: data
+      });
+    });
 });
 
 module.exports = router;
